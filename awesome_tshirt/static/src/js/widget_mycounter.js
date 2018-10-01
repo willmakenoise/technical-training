c25d0cd0984f763c2646cf184868f98d50d5bd00
Marius Stedjan <marius@stedjan.com>
2018-10-01 12:40:21 +0200

    Fix

--- awesome_tshirt/static/src/js/widget_mycounter.js
+++ awesome_tshirt/static/src/js/widget_mycounter.js
@@ -4,17 +4,27 @@ odoo.define('awesome_tshirt.MyCounter', function(require) {
     var MyCounter = Widget.extend({
         template: 'widget_mycounter',
         events: {
-            'click button.o_increment': '_onClick',
+            // 'click button.o_increment': '_onClick',
+            'click .oe_decrement': '_onDecrement',
+            'click .oe_increment': '_onIncrement',
         },
         init: function (parent, value) {
             this._super(parent);
             this.count = value;
         },
-        _onClick: function (e) {
-            this.count++;
-            // this.$('.count').text(this.count);
+        // _onClick: function (e) {
+        //     this.count++;
+        //     // this.$('.count').text(this.count);
+        //     this.renderElement();
+        // },
+        _onDecrement: function () {
+            this.count--;
             this.renderElement();
-        }
+        },
+        _onIncrement: function () {
+            this.count++;
+            this.renderElement();
+        },
     });
 
     return MyCounter;
