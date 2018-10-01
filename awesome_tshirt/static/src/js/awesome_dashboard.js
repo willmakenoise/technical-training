0c426385f9f4cf5993d2a796c6befb1b20bee234
Marius Stedjan <marius@stedjan.com>
2018-10-01 14:26:20 +0200

    create myActions

--- awesome_tshirt/static/src/js/awesome_dashboard.js
+++ awesome_tshirt/static/src/js/awesome_dashboard.js
@@ -2,15 +2,24 @@ odoo.define('awesome_tshirt.dashboard', function(require) {
     var AbstractAction = require('web.AbstractAction');
     var core = require('web.core');
     var MyCounter = require('awesome_tshirt.MyCounter');
-    // var ServicesMixin = require('web.ServicesMixin');
+    var MyActions = require('awesome_tshirt.MyActions');
 
     var Dashboard = AbstractAction.extend({
         start: function () {
-            var myCounter = new MyCounter(this, 0);
-            var counterDef = myCounter.appendTo(this.$el);
-            var superDef = this._super.apply(this, arguments);
-            return $.when(counterDef, superDef);
-        },        
+            // Counter Widget
+            // var myCounter = new MyCounter(this, 0);
+            // var counterDef = myCounter.appendTo(this.$el);
+            // var superDef = this._super.apply(this, arguments);
+            // return $.when(counterDef, superDef);
+
+            // Time JS library; moment
+
+            // Actions Widget
+            var myActions = new MyActions(this);
+            var actionsDef = myActions.appendTo(this.$el);
+            var superDef =this._super.apply(this, arguments);
+            return $.when(actionsDef, superDef);
+        },
     });
 
     core.action_registry.add('awesome_tshirt.dashboard', Dashboard);
