3ff41202d1178ac249a2a388dc2d4b5d31d1bb10
Marius Stedjan <marius@stedjan.com>
2018-10-02 14:49:11 +0200

    Fix2

--- awesome_tshirt/static/src/js/order_form_view.js
+++ awesome_tshirt/static/src/js/order_form_view.js
@@ -1,14 +1,19 @@
 odoo.define('awesome_tshirt.order_form_view', function(require) {
     "use strict";
+
     /**
      * This file defines a custom FormView for the model awesome_tshirt.order,
      * which adds a 'Print label' button to the ControlPanel.
      */
+
     var core = require('web.core');
     var FormController = require('web.FormController');
     var FormView = require('web.FormView');
     var viewRegistry = require('web.view_registry');
+
+    var _t = core._t;
     var qweb = core.qweb;
+
     var OrderFormController = FormController.extend({
         /**
          * @override
@@ -19,9 +24,11 @@ odoo.define('awesome_tshirt.order_form_view', function(require) {
             // the label (e.g. if the user double-clicks on the button)
             this.printing = false;
         },
+
         //--------------------------------------------------------------------------
         // Public
         //--------------------------------------------------------------------------
+
         /**
          * Overrides to add the 'Print Label' button to the ControlPanel
          *
@@ -33,9 +40,11 @@ odoo.define('awesome_tshirt.order_form_view', function(require) {
             this.$buttons.append(qweb.render('OrderFormView.Buttons'));
             this.$buttons.on('click', '.o_print_label', this._onPrintLabel.bind(this));
         },
+
         //--------------------------------------------------------------------------
         // Private
         //--------------------------------------------------------------------------
+
         /**
          * Overrides to update the style and disabled status of the custom 'Print
          * Label' button.
@@ -47,16 +56,18 @@ odoo.define('awesome_tshirt.order_form_view', function(require) {
                     raw: true
                 });
                 var disabled = this.mode === 'edit' && !state.res_id;
-                var primary = state.data.customer_id && state.data.state !== 'printed';
+                var primary = state.data.customer_id && state.data.state === 'printed';
                 this.$buttons.find('.o_print_label')
                     .toggleClass('btn-primary', primary)
                     .toggleClass('btn-secondary', !primary)
                     .attr('disabled', !!disabled);
             }
         },
+
         //--------------------------------------------------------------------------
         // Handlers
         //--------------------------------------------------------------------------
+
         /**
          * @private
          */
@@ -73,16 +84,27 @@ odoo.define('awesome_tshirt.order_form_view', function(require) {
                 model: 'awesome_tshirt.order',
                 method: 'print_label',
                 args: [res_id],
-            }).then(function() {
-                self.printing = false;
+            }).then(function(printed) {
+                if (printed) {
+                    self.do_notify(_t('Success'), _t('The label has been printed'));
+                } else {
+                    self.do_warn(_t('Failure'), _t('The label cannot be printed'), {
+                        sticky: true
+                    });
+                }
                 self.reload();
+            }).always(function() {
+                self.printing = false;
             });
         },
     });
+
     var OrderFormView = FormView.extend({
         config: _.extend({}, FormView.prototype.config, {
             Controller: OrderFormController,
         }),
     });
+
     viewRegistry.add('order_form_view', OrderFormView);
+
 });
