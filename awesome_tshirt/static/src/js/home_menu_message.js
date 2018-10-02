4570eb35842af58309b133a335055360a9e2127e
Marius Stedjan <marius@stedjan.com>
2018-10-02 15:50:21 +0200

    2

--- awesome_tshirt/static/src/js/home_menu_message.js
+++ awesome_tshirt/static/src/js/home_menu_message.js
@@ -1,10 +1,11 @@
 odoo.define('awesome_tshirt.home_menu_message', function(require) {
     var HomeMenu = require('web_enterprise.HomeMenu');
+    var qweb = require('core.qweb');
 
     HomeMenu.include({
         render: function () {
             this._super();
-            var message = $(QWeb.render('awesome_tshirt.home_menu_message', {
+            var message = $(qweb.render('awesome_tshirt.home_menu_message', {
             })).insertBefore(self.$menuSearch);
         },
     });
